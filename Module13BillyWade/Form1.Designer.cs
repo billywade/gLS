c541d6bcb44b69f8ed74a8658a495906466c0fb9
Trey Merkley <billy.wade@protonmail.ch>
2018-05-01 09:39:16 -0500

    initial commit

---
+++ Module13BillyWade/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Module13BillyWade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.listButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(12, 13);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.ReadOnly = true;
+            this.pathTextBox.Size = new System.Drawing.Size(341, 20);
+            this.pathTextBox.TabIndex = 0;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(359, 11);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // listButton
+            // 
+            this.listButton.Location = new System.Drawing.Point(116, 282);
+            this.listButton.Name = "listButton";
+            this.listButton.Size = new System.Drawing.Size(75, 23);
+            this.listButton.TabIndex = 5;
+            this.listButton.Text = "List";
+            this.listButton.UseVisualStyleBackColor = true;
+            this.listButton.Click += new System.EventHandler(this.listButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(12, 39);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputTextBox.Size = new System.Drawing.Size(422, 237);
+            this.outputTextBox.TabIndex = 6;
+            this.outputTextBox.WordWrap = false;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(278, 282);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 7;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(359, 282);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(197, 282);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 9;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 317);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.listButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.pathTextBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "gLS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+      //  private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button listButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Module13BillyWade/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Module13BillyWade
+{
+    public partial class Form1 : Form
+    {
+        string dir = "";
+        string liststring = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            pathTextBox.Text = "";
+            dir = "";
+            outputTextBox.Text = "";
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            DialogResult result = folderDlg.ShowDialog();
+            pathTextBox.Text = folderDlg.SelectedPath;
+            dir = folderDlg.SelectedPath;
+            Environment.SpecialFolder root = folderDlg.RootFolder;
+
+        }
+
+        private void listButton_Click(object sender, EventArgs e)
+        {
+            outputTextBox.AppendText("starting...." + Environment.NewLine);
+            liststring = "";
+            backgroundWorker1.RunWorkerAsync();
+            while (this.backgroundWorker1.IsBusy)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void List(string dir, DoWorkEventArgs e)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(dir))
+                    liststring += (f + Environment.NewLine);
+                if (backgroundWorker1.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    return; // abort work, if it's cancelled
+                }
+                foreach (string g in Directory.GetDirectories(dir))
+                {
+                    liststring += (g + Environment.NewLine);
+                    List(g, e);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                liststring += ex.Message;
+            } 
+        }
+ 
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                outputTextBox.AppendText("cancelled.");
+                    backgroundWorker1.CancelAsync();
+            }
+
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List(dir, e);
+            BeginInvoke((MethodInvoker)delegate
+            {
+                outputTextBox.Text = liststring; 
+            });
+        }
+    }
+}
+
